4620ae8ef28699b450383aee2783257b615248c0
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-06 14:06:09 +0200

    Adding Blog Routes 🚀

---
+++ routes/blog.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.redirect('/posts'));
+router.get('/posts', (req, res) => res.render('posts-list'));
+router.get('/new-post', (req, res) => res.render('create-post'));
+
+module.exports = router;
