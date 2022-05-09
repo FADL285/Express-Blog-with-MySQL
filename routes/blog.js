89021ded28de9890aecf51389124b7f31adaa97f
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-09 03:11:24 +0200

    Install & Configure Prettier

--- routes/blog.js
+++ routes/blog.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const db = require('../db');
+const express = require("express");
+const db = require("../db");
 
 const router = express.Router();
-router.get('/', (req, res) => res.redirect('/posts'));
+router.get("/", (req, res) => res.redirect("/posts"));
 
-router.get('/posts', async (req, res) => {
+router.get("/posts", async (req, res) => {
   const query = `SELECT posts.*, authors.name AS author FROM posts INNER JOIN authors ON posts.author_id = authors.id`;
   const [posts] = await db.query(query);
-  res.render('posts-list', { posts });
+  res.render("posts-list", { posts });
 });
 
-router.get('/new-post', async (req, res) => {
-  const [authors] = await db.query('SELECT * FROM authors');
-  res.render('create-post', { authors });
+router.get("/new-post", async (req, res) => {
+  const [authors] = await db.query("SELECT * FROM authors");
+  res.render("create-post", { authors });
 });
 
-router.post('/posts', async (req, res) => {
+router.post("/posts", async (req, res) => {
   const data = [
     req.body.title,
     req.body.summary,
@@ -26,7 +26,7 @@ router.post('/posts', async (req, res) => {
     `INSERT INTO posts (title, summary, body, author_id) VALUES (?)`,
     [data]
   );
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
