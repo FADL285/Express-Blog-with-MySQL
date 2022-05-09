6bf29e4a93e3606f1f967047426513e646b80b85
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-09 03:05:14 +0200

    Fetch Posts and Display it on Home Page.

--- routes/blog.js
+++ routes/blog.js
@@ -4,7 +4,11 @@ const db = require('../db');
 const router = express.Router();
 router.get('/', (req, res) => res.redirect('/posts'));
 
-router.get('/posts', (req, res) => res.render('posts-list'));
+router.get('/posts', async (req, res) => {
+  const query = `SELECT posts.*, authors.name AS author FROM posts INNER JOIN authors ON posts.author_id = authors.id`;
+  const [posts] = await db.query(query);
+  res.render('posts-list', { posts });
+});
 
 router.get('/new-post', async (req, res) => {
   const [authors] = await db.query('SELECT * FROM authors');
